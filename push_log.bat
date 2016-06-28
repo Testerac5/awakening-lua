e0cbf29db697ed116c710144a62e22e45acd30c6
drdread987 <thatonejosh1996@gmail.com>
2016-06-27 19:46:46 -0500

    Delete push_log.bat

--- push_log.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\Administrator\Desktop\Awakening\lua_scripts
-git add Eluna.log
-git commit -m updating_log
-git push https://drdread987:slayer1996@github.com/Serenity-/Awakening_LUA_Master.git
