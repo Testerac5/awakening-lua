72891c6f8d38d623b7668b1a65d5c4776614b111
Sheldon <jangshelldon@gmail.com>
2016-08-29 03:11:08 +0300

    hunger fix

--- hunger_system/HungerClient.lua
+++ hunger_system/HungerClient.lua
@@ -21,18 +21,8 @@ frame:SetToplevel(false)
 frame:SetClampedToScreen(true)
 frame:SetMovable(true)
 frame:EnableMouse(true)
-frame:SetBackdrop({
-            bgFile = "Interface\\AddOns\\AwAddons\\Textures\\misc\\WowUI_Horizontal_Frame",
-            --tile = true
-            })  
 frame:SetScript("OnDragStart", frame.StartMoving)   
 frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
-
-
-
-
-
-AIO.SavePosition(frame)
 
 local Food_Ico = frame:CreateTexture(nil, "ARTWORK")
         Food_Ico:SetWidth(26);               
@@ -47,22 +37,31 @@ local Hunger_BackGround = frame:CreateTexture(nil, "BACKGROUND")
         Hunger_BackGround:SetVertexColor(1,1,1,0.6)
         Hunger_BackGround:SetPoint("CENTER")
 
+local Hunger_BackGround_edge = CreateFrame("Frame", "Hunger_BackGround_edge", frame,nil)
+        Hunger_BackGround_edge:SetSize(frame:GetSize())
+        Hunger_BackGround_edge:SetBackdrop({bgFile = "Interface\\AddOns\\AwAddons\\Textures\\misc\\WowUI_Horizontal_Frame"});
+        Hunger_BackGround_edge:SetPoint("CENTER")
+        Hunger_BackGround_edge:SetFrameStrata("HIGH")
 
-local second_frame = CreateFrame("Frame", "Second_Frame", frame)
+
+
+
+
+AIO.SavePosition(frame)
+
+
+second_frame = CreateFrame("StatusBar", "Second_Frame", frame)
 second_frame:SetSize(105,19)
 second_frame:RegisterForDrag("LeftButton")
 second_frame:SetPoint("CENTER")
 second_frame:SetToplevel(true)
 second_frame:SetClampedToScreen(true)
 second_frame:SetMovable(false)
-second_frame:EnableMouse(false)
 second_frame:SetFrameStrata("MEDIUM")
-second_frame:SetBackdrop({
-            bgFile = "Interface\\TargetingFrame\\UI-StatusBar",
-            --tile = true,
-            insets = { left = 1, right = 1, top = 1, bottom = 1 }
-            })  
-second_frame:SetBackdropColor(1,0,.7,0.8)
+second_frame:EnableMouse(false)
+second_frame:SetMinMaxValues(0, 100)
+second_frame:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")  
+second_frame:SetStatusBarColor(1,0,.7,0.8)
 local function frame2_Tooltip_OnEnter(self, motion)
             GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
             GameTooltip:SetText("Hunger Bar\nEat food to raise it!")
@@ -80,35 +79,35 @@ function MyHandlers.GetHungerPct(player,current_hunger)
 
 	local level = UnitLevel("player")
 
-	
-	local max_hunger = (level * 5) + 100
-	
-	local rough_pct = ((current_hunger ) / max_hunger) * 100
+    
+    local max_hunger = (level * 5) + 100
+    
+    local rough_pct = ((current_hunger ) / max_hunger) * 100
 
-	
-	local percent =  rough_pct - (rough_pct % 1)
-	
-	if percent <= 0 then
+    
+    local percent =  rough_pct - (rough_pct % 1)
+    
+    if percent <= 0 then
     
         print("|cffFFFF00I am extremely hungry, and I am beginning to starve.|r") -- edited
     
     elseif percent < 25 then
     
-        second_frame:SetBackdropColor(1,.35,0,0.8)
+        second_frame:SetStatusBarColor(1,.35,0,0.8)
         print("|cffFFFF00I must eat soon or I will start taking damage.|r") -- edited
     
     elseif percent < 65 then
     
-        second_frame:SetBackdropColor(.9,.9,.3,0.8)
+        second_frame:SetStatusBarColor(.9,.9,.3,0.8)
     
     end
     
     if percent > 65 then
-        second_frame:SetBackdropColor(1,0,.7,0.8)
+        second_frame:SetStatusBarColor(1,0,.7,0.8)
     end
-	
-	
-	second_frame:SetSize(percent, 19)
+    
+    
+    second_frame:SetValue(percent)
 
 end
 
