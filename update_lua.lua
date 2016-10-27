a0fbcb6cc7fc663ec192530fb498afd2ed97fa6f
Derek <derekpowellofficial@gmail.com>
2016-10-27 18:53:42 -0500

    Update LUA print fix.

--- update_lua.lua
+++ update_lua.lua
@@ -5,20 +5,23 @@ function update(event, player, msg, Type, lang)
     if player_type == 3 then 
 	
 		if msg == "%push lua" then
-    
+		
 			os.execute('./update_lua.sh')
+			player:SendBroadcastMessage("Successfully pushed Lua Scripts. Some Modules require restarts or .reload eluna")
 			
 		
 		
 		elseif msg == "%push log" then
 		
 			os.execute('./push_log.sh')
+			player:SendBroadcastMessage("Successfully pushed Lua Log")
 		
 		
 		
 		elseif msg == "%push dbc" then
 		
 			os.execute('./update_dbc.sh')
+			player:SendBroadcastMessage("Successfully pushed DBC Files. This requires a restart")
 		
 		end
     
