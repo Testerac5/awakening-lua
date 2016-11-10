c8522e9c4750514fade6c414fc92855a63b98e3a
Sheldon <jangshelldon@gmail.com>
2016-11-11 00:11:21 +0300

    Still trying to figure our what is wrong with the lack of tests ._.

--- EnchantReRoll/EnchantReRollC.lua
+++ EnchantReRoll/EnchantReRollC.lua
@@ -124,8 +124,8 @@ local name, itemlink, _, _, _, _, _, _, _, texture, _ = GetItemInfo(item)
 ClearCursor()
 EnchantReRollMain_Item.Button:SetNormalTexture(texture)
 EnchantReRollMain.item = item
---EnchantReRollMain.itemEffectName = GetSpellInfo(nameeffect)
-EnchantReRollMain.itemEffect = "|Hspell:"..nameeffect.."|h[Test]|h"
+EnchantReRollMain.itemEffectName = nameeffect
+EnchantReRollMain.itemEffect = "|Hspell:"..nameeffect.."|h["..nameeffect.."]|h"
 EnchantReRollMain.itemCost = cost
 --For Reforge
 EnchantReRollMain_RollButton.Slot = slot
