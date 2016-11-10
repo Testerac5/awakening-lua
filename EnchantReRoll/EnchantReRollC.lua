2f2f1fb40cdcf8e5884621faa1c99f6065686193
Sheldon <jangshelldon@gmail.com>
2016-11-11 00:01:39 +0300

    I hope the last fix for enchant Reroll visual part

--- EnchantReRoll/EnchantReRollC.lua
+++ EnchantReRoll/EnchantReRollC.lua
@@ -124,8 +124,8 @@ local name, itemlink, _, _, _, _, _, _, _, texture, _ = GetItemInfo(item)
 ClearCursor()
 EnchantReRollMain_Item.Button:SetNormalTexture(texture)
 EnchantReRollMain.item = item
-EnchantReRollMain.itemEffectEntry = nameeffect
-EnchantReRollMain.itemEffect = GetSpellLink(EnchantReRollMain.itemEffectEntry)
+EnchantReRollMain.itemEffectName = GetSpellInfo(nameeffect)
+EnchantReRollMain.itemEffect = "|Hspell:"..nameeffect.."|h["..EnchantReRollMain.itemEffectName.."]|h"
 EnchantReRollMain.itemCost = cost
 --For Reforge
 EnchantReRollMain_RollButton.Slot = slot
