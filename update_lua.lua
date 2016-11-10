0fd28f7d8cbe00001058dd4812c121b76462d369
Carlos Correa <razor950@gmail.com>
2016-11-10 17:38:12 -0500

    Push Commands should be working now, however you need to do %push dbc sargeras or %push lua sargeras or %push dbc proudmoore  etc)

--- update_lua.lua
+++ update_lua.lua
@@ -4,36 +4,48 @@ function update(event, player, msg, Type, lang)
     
     if player_type == 3 then 
 	
-		if msg == "%push lua" then
+		if msg == "%push lua sargeras" then
 		
-			os.execute ('bash update_lua.sh')
+			os.execute ('bash /home/develop/awake/main/bin/lua_scripts/update_lua.sh')
 			player:SendBroadcastMessage("Successfully pushed Lua Scripts. Some Modules require restarts or .reload eluna")
 		
-		elseif msg == "%push log" then
+		elseif msg == "%push log sargeras" then
 		
-			os.execute ('bash push_log.sh')
+			os.execute ('bash /home/develop/awake/main/bin/lua_scripts/push_log.sh')
 			player:SendBroadcastMessage("Successfully pushed Lua Log")
 		
-		elseif msg == "%push dbc" then
+		elseif msg == "%push dbc sargeras" then
 		
-			os.execute ('bash update_dbc.sh')
+			os.execute ('bash /home/develop/awake/main/bin/lua_scripts/update_dbc.sh')
 			player:SendBroadcastMessage("Successfully pushed DBC Files. This requires a restart")
 		
-		elseif msg == "%push live" then
+		elseif msg == "%push sargeras" then
 		
-			os.execute ('bash /home/develop/compile.sh &')
+			os.execute ('bash /home/develop/compile.sh')
 			player:SendBroadcastMessage("Successfully started compiling core. This requires a restart but please wait a few minutes for compiling to finish.")
-			
-		elseif msg == "%push dev" then
+
+		elseif msg == "%push lua proudmoore" then
 		
-			os.execute ('bash /home/develop/develop.sh &')
+			os.execute ('bash /home/develop/awake/proudmoore/bin/lua_scripts/update_lua.sh')
+			player:SendBroadcastMessage("Successfully pushed Lua Scripts. Some Modules require restarts or .reload eluna")
+		
+		elseif msg == "%push log proudmoore" then
+		
+			os.execute ('bash /home/develop/awake/proudmoore/bin/lua_scripts/push_log.sh')
+			player:SendBroadcastMessage("Successfully pushed Lua Log")
+		
+		elseif msg == "%push dbc proudmoore" then
+		
+			os.execute ('bash /home/develop/awake/proudmoore/bin/lua_scripts/update_dbc.sh')
+			player:SendBroadcastMessage("Successfully pushed DBC Files. This requires a restart")
+			
+		elseif msg == "%push proudmoore" then
+		
+			os.execute ('bash /home/develop/develop.sh')
 			player:SendBroadcastMessage("Successfully started compiling core. This requires a restart but please wait a few minutes for compiling to finish.")
 
 		end
-    
     end
-	
-
 end
 
 
