f7c3fddae09fb9a4cbf30580bb50e75073e3feb2
EpitomeRP <epitomerp@gmail.com>
2016-12-16 16:58:07 -0700

    GBOM #2

--- extra_buttons_bar/character advancement/paladin_vari.lua
+++ extra_buttons_bar/character advancement/paladin_vari.lua
@@ -32,7 +32,7 @@ paladin_holy_talents_tab = 26
 paladin_retribution_spells = {{19740, 2, 0, 4}, {20271, 2, 0 ,4}, {53408, 2, 0, 12}, {7294, 2, 0, 16}, {53407, 2, 0, 28},
 {24275, 2, 0, 44}, {32223, 2, 0, 62}, {31801, 2, 0, 64}, {31884, 2, 0, 70}}
 
-paladin_retribution_spells_count = 10
+paladin_retribution_spells_count = 9
 paladin_retribution_bgs = {"Interface\\TalentFrame\\Paladincombat-TopLeft", "Interface\\TalentFrame\\Paladincombat-TopRight",
 "Interface\\TalentFrame\\Paladincombat-BottomLeft", "Interface\\TalentFrame\\Paladincombat-BottomRight"}
 
