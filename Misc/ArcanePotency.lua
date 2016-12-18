c04bb52671681917f7802b2c2b19b77e6b893c4c
EpitomeRP <epitomerp@gmail.com>
2016-12-18 04:21:35 -0700

    Arcane Potency #2

--- Misc/ArcanePotency.lua
+++ Misc/ArcanePotency.lua
@@ -1,6 +1,6 @@
 local function DivineDebuff(event, player, spell, skipCheck)
 	if (spell:GetEntry() == 12536) and (player:HasSpell(31572) == true)
-		player:AddAura(57531)
+		player:AddAura(57531, self)
 	end
 end
 
