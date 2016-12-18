251f6fd42f0129982763b8f7298f918ad4f6203d
EpitomeRP <epitomerp@gmail.com>
2016-12-18 04:31:46 -0700

    Arcane Potency #4

--- Misc/ArcanePotency.lua
+++ Misc/ArcanePotency.lua
@@ -1,6 +1,7 @@
 local function Potency(event, player, spell, skipCheck)
-	if (spell:GetEntry() == 12536) and (player:HasSpell(31572) == true)
+	if (spell:GetEntry() == 12536) and (player:HasSpell(31572) == true) then
 		player:AddAura(57531, self)
+		
 	end
 end
 
