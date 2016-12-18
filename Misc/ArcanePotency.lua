6ae51177599d01564a11254fc2f14af3f18b40c9
EpitomeRP <epitomerp@gmail.com>
2016-12-18 04:48:35 -0700

    Remove script

--- Misc/ArcanePotency.lua
+++
@@ -1,8 +0,0 @@
-local function Potency(event, player, spell, skipCheck)
-	if (spell:GetEntry() == 12536) and (player:HasSpell(31572) == true) then
-		player:AddAura(57531, player)
-		player:SendBroadcastMessage("test")
-	end
-end
-
-RegisterPlayerEvent(5, Potency)
