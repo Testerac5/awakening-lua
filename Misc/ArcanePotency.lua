7707f81bbd6964a7fce53bc17d15179132cb0863
EpitomeRP <epitomerp@gmail.com>
2016-12-18 04:27:07 -0700

    Arance Potency #3

--- Misc/ArcanePotency.lua
+++ Misc/ArcanePotency.lua
@@ -1,7 +1,7 @@
-local function DivineDebuff(event, player, spell, skipCheck)
+local function Potency(event, player, spell, skipCheck)
 	if (spell:GetEntry() == 12536) and (player:HasSpell(31572) == true)
 		player:AddAura(57531, self)
 	end
 end
 
-RegisterPlayerEvent(5, DivineDebuff)
+RegisterPlayerEvent(5, Potency)
