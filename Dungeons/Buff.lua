83605c9911b682a908f5d6665bebaac9822dbe07
EpitomeRP <epitomerp@gmail.com>
2016-12-31 15:57:12 -0700

    Buff #2
    updated for multiple dungeons based on query

--- Dungeons/Buff.lua
+++ Dungeons/Buff.lua
@@ -6,22 +6,25 @@ end
 local timing = false
 
 function Buff(event, player)
-	-- check for player map id
-	if (player:GetMapId() == 48) then
-		-- make sure we dont dupe the same timer 10000 times
+	-- query to select spell and map
+	local Buff1 = WorldDBQuery("SELECT map, spell FROM instance_buff WHERE map = " ..player:GetMapId().. ";")
+	if (Buff1 ~= nil) then
+		--so we dont dupe the same timer 100000 times
 		if (timing == false) then
-			player:RegisterEvent(Redo, 5000, 0)
+			player:RegisterEvent(Redo, 120000, 0)
 			--DEBUG--player:SendBroadcastMessage("timing now")
 			timing = true
 		end
 	local mobs = player:GetCreaturesInRange(533)
+	local buffspell = Buff1:GetInt32(1)
+		-- add aura based on DB query above and loop for mobs in range
 		for x=1,#mobs,1 do
-			-- loop to check all mobs in range of 533, add aura ID
-			if mobs[x]:HasAura(465) == false then
-				mobs[x]:AddAura(465, mobs[x])
+			if mobs[x]:HasAura(buffspell) == false then
+				mobs[x]:AddAura(buffspell, mobs[x])
 			end
 		end
 	else
+		-- do this because they arent in a buffed instance
 		timing = false
 		--DEBUG--player:SendBroadcastMessage("stopped timing")
 	end
