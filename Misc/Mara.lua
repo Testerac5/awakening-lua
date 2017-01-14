19c0facacc88234bad6773d5eb3cf45257030a03
EpitomeRP <epitomerp@gmail.com>
2017-01-14 15:19:09 -0700

    Mara Port #4
    Fixed mara port and did various organization within the folder

--- Misc/Mara.lua
+++
@@ -1,6 +0,0 @@
-local function MaraStaff(event, player, item, target)
-	item:GetGameObjectsInRange( 1, 178386)
-	player:CastSpell(player, 21127, false)
-end
-
-RegisterItemEvent(17191, 2, MaraStaff)
