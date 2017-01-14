3135b3e78b8ba07e608455cb93f2a745c56013a7
EpitomeRP <epitomerp@gmail.com>
2017-01-14 12:10:00 -0700

    Merged two scripts since they served a very similar purpose

--- Misc/ThanksFounder.lua
+++
@@ -1,6 +0,0 @@
-local function fam(event, player, item, count)
-	player:SendNotification("|cffFF6600 Thanks for becoming a founder! |r")
-	player:CastSpell(player, 55420, false)
-end
-	
-RegisterItemEvent(977020, 2, fam)
