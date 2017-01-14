adc21f3d063570113234e7ed5dedaf38680ae484
EpitomeRP <epitomerp@gmail.com>
2017-01-14 16:03:32 -0700

    Scholo Gambit #1

---
+++ Misc/Scholo.lua
@@ -0,0 +1,22 @@
+local function SpookyItUp(eventId, delay, repeats, player)
+	player:GetCreaturesInRange(100, 10475, 0, 0)
+	for x=1,#mobs,1 do
+		local x = mobs[x]:GetX()
+		local y = mobs[x]:GetY()
+		local z = mobs[x]:GetZ()
+		local o = mobs[x]:GetO()
+		if mobs[x]:IsDead() == false then
+			mobs[x]:Kill(mobs[x])
+		end
+		mobs[x]:SpawnCreature(11547, x, y, z, o, 6, 60000)
+	end
+end
+
+local function Spooky(event, player, item, target)
+	local px = player:GetX()
+	local gambit = player:GetNearestGameobject(10, 177304)
+	gambit:RemoveFromWorld(true)
+	player:RegisterEvent(14000, funk, 0)
+end
+
+RegisterItemEvent(17191, 2, MaraStaff)
