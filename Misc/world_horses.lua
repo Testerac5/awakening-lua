73fa6119a42f6acda1741fc7a738881fb920e7c6
Sheldon <jangshelldon@gmail.com>
2017-01-27 15:16:22 +0300

    one more update of my old scirpt for horses

--- Misc/world_horses.lua
+++ Misc/world_horses.lua
@@ -1,15 +1,15 @@
 ﻿local horse_id = {5403, 5406, 5405, 5404, 12376, 14559, 15460, 14561, 28899, 4269, 27214}
-local num = 0
+
 local function HourseCustomSpell(event, creature, unit)
-	num = num+1
-if (num == 210) then
+	creature.num = creature.num+1
+if (creature.num == 140) then
 	local customspellornot = math.random(1, 2)
 		if (customspellornot == 1) then
 				creature:RemoveAura(55587)
 		elseif (customspellornot == 2) then
 				creature:AddAura(55587, creature)
 		end
-num = 0
+creature.num = 0
 end
 end
 for k, v in pairs(horse_id) do
