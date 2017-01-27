2ad89666c013e912a0384c4c54fd69eeafe11b99
Sheldon <jangshelldon@gmail.com>
2017-01-27 14:39:00 +0300

    tests with molten core script

--- Dungeons/MoltenCore/Magmadar.lua
+++ Dungeons/MoltenCore/Magmadar.lua
@@ -1,12 +1,13 @@
---[[local BossEntry_Magmadar = 11982
+local BossEntry_Magmadar = 11982
 local GameObject_LavaBomb1 = 19411
 local GameObject_LavaBomb2 = 20474
 local Spell_LavaBomb = 177704
 
 local function LavaBombSpawn(event, creature, target, spellid)
 	if (spellid == Spell_LavaBomb) then
-		PerformIngameSpawn( 2, GameObject_LavaBomb1, target:GetMapId(), target:GetInstanceId(), target:GetX(), target:GetY(), target:GetZ(), target:GetO(), false, 1800, target:GetPhaseMask())
+		target:SendBroadcastMessage("Lavabomb casted on you!")
+		--PerformIngameSpawn( 2, GameObject_LavaBomb1, target:GetMapId(), target:GetInstanceId(), target:GetX(), target:GetY(), target:GetZ(), target:GetO(), 0, 1800, target:GetPhaseMask())
 	end
 end
 
-RegisterCreatureEvent(BossEntry_Magmadar, 15, LavaBombSpawn)]]--
+RegisterCreatureEvent(BossEntry_Magmadar, 15, LavaBombSpawn)
