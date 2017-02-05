15c0d1eeb4e98b6da427c487071de359fc7f00c2
EpitomeRP <epitomerp@gmail.com>
2017-02-05 15:47:45 -0700

    ZF Gong Script

---
+++ Misc/items/ZFGong.lua
@@ -0,0 +1,10 @@
+local function ZFGong(event, player, item, target)
+	local goober = player:GetNearestGameObject( 3, 141832)
+	if (goober == nil) then
+		return false;
+	else
+		player:SpawnCreature(7273, 1665.56, 1187.32, 6.64, 3.9, 7)
+	end
+end
+
+RegisterItemEvent(17191, 2, ZFGong)
