99ef4a714c11fe7a78999d5a0d09e4ad4311842e
EpitomeRP <epitomerp@gmail.com>
2017-02-04 18:02:10 -0700

    Remove script
    didnt work :( broke smart  scripts

--- Misc/spells/HitObject.lua
+++
@@ -1,8 +0,0 @@
---local function ObjectTimer
---end
-
-local function HitObject(event, creature, target, spellid)
-	target:SendBroadcastMessage("hit")
-end
-
-RegisterCreatureEvent(11982, 15, HitObject)
