0863890c362e5c8ce3eacabf3bf5fa177ff025a7
Sheldon <jangshelldon@gmail.com>
2017-02-07 02:43:41 +0300

    Inn bug fixes

--- Misc/SanctuaryFix.lua
+++ Misc/SanctuaryFix.lua
@@ -11,9 +11,26 @@
 
 ]]
 
-local function sanctuary_fix_2(event, player, newZone, newArea) -- using to prevent saving flags after leaving the inn
+local function sanctuary_fix_2(eventId, delay, repeats, player) -- using to prevent saving flags after leaving the inn
 if not(player:IsRested()) and not(player:IsFFAPVP()) then
 	player:SetFFA(true)
 end
+
+if player:IsInCombat() and player:GetVictim() then
+	if not player:GetVictim():ToPlayer() then
+		return false
+	end
+	local victim = player:GetVictim():ToPlayer()
+	if ((player:IsAlliance() and victim:IsAlliance()) or (player:IsHorde() and victim:IsHorde())) then
+		if not(victim:IsFFAPVP()) then
+			player:ClearInCombat()
+		end
+	end
+	end
 end
-RegisterPlayerEvent(27, sanctuary_fix) -- zone change
+
+local function TimingChecks(event,player)
+	player:RegisterEvent(sanctuary_fix_2, 2000, 0)
+end
+
+RegisterPlayerEvent(3, TimingChecks)
