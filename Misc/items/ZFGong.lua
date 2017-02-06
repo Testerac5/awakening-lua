1665fda585bb70db90f4e68afa4260147cf59a9b
EpitomeRP <epitomerp@gmail.com>
2017-02-06 01:05:59 -0700

    ZF Gong #8
    maybe this works???

--- Misc/items/ZFGong.lua
+++ Misc/items/ZFGong.lua
@@ -1,16 +1,14 @@
 local function ZFGong(event, player, item, target)
-	local goober = player:GetNearestGameObject( 3, 141832)
+	local goober = player:GetNearestGameObject( 5, 141832)
 	local dupe = player:GetCreaturesInRange(100, 7273, 0, 0)
 	if (goober == nil) then
 		player:SendBroadcastMessage("no goober")
 		return false;
-	elseif (dupe ~= 0) then
-		player:SendBroadcastMessage("there are " ..dupe:GetRowCount().. " ghazrillas.")
-		return false;
 	else
-		player:SpawnCreature(7273, 1665.56, 1187.32, 6.64, 3.9, 7)
+		player:SpawnCreature(7273, 1665.56, 1187.32, 11.64, 3.9, 7)
 		player:SendBroadcastMessage("gong used")
 	end
 end
+
 
 RegisterItemEvent(9240, 2, ZFGong)
