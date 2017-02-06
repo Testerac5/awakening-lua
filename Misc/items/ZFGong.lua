4807b3182500ac70ff5627abc49dd01ee1e5c7b8
EpitomeRP <epitomerp@gmail.com>
2017-02-05 21:22:41 -0700

    ZF Gong #6
    more debug info

--- Misc/items/ZFGong.lua
+++ Misc/items/ZFGong.lua
@@ -1,10 +1,11 @@
 local function ZFGong(event, player, item, target)
 	local goober = player:GetNearestGameObject( 3, 141832)
+	local dupe = player:GetCreaturesInRange(100, 7273, 0, 0)
 	if (goober == nil) then
 		player:SendBroadcastMessage("no goober")
 		return false;
-	elseif (player:GetCreaturesInRange(100, 7273, 0, 0) ~= 0) then
-		player:SendBroadcastMessage("multiple boss")
+	elseif (dupe ~= 0) then
+		player:SendBroadcastMessage("there are " ..tostring(dupe).. " ghazrillas.")
 		return false;
 	else
 		player:SpawnCreature(7273, 1665.56, 1187.32, 6.64, 3.9, 7)
