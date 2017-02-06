8e9fc1f1a5ef45ba84a840b3b3925d73f54d5e24
EpitomeRP <epitomerp@gmail.com>
2017-02-06 01:20:18 -0700

    ZF Gong #9
    "It just works." -Todd Howard

--- Misc/items/ZFGong.lua
+++ Misc/items/ZFGong.lua
@@ -1,12 +1,9 @@
 local function ZFGong(event, player, item, target)
 	local goober = player:GetNearestGameObject( 5, 141832)
-	local dupe = player:GetCreaturesInRange(100, 7273, 0, 0)
 	if (goober == nil) then
-		player:SendBroadcastMessage("no goober")
 		return false;
 	else
 		player:SpawnCreature(7273, 1665.56, 1187.32, 11.64, 3.9, 7)
-		player:SendBroadcastMessage("gong used")
 	end
 end
 
