ea94c9f1d6147e20b94dcc429ccbaf72ab9cf138
Sheldon <jangshelldon@gmail.com>
2017-02-07 23:36:04 +0300

    Inn bug fix, I need damn logs to work with that

--- Misc/SanctuaryFix.lua
+++ Misc/SanctuaryFix.lua
@@ -11,7 +11,7 @@
 
 ]]
 
-local function sanctuary_fix_2(eventId, delay, repeats, player) -- using to prevent saving flags after leaving the inn
+--[[local function sanctuary_fix_2(eventId, delay, repeats, player) -- using to prevent saving flags after leaving the inn
 if (not(player:IsRested()) and not(player:IsFFAPVP())) then
 	player:SetFFA(true)
 end
@@ -28,11 +28,11 @@ player:SendBroadcastMessage("test") -- DEBUG
 			player:ClearInCombat()
 		end
 	end
-	end]]--
+	end
 end
 
 local function TimingChecks(event,player)
 	player:RegisterEvent(sanctuary_fix_2, 2000, 0)
 end
 
-RegisterPlayerEvent(3, TimingChecks)
+RegisterPlayerEvent(3, TimingChecks)]]--
