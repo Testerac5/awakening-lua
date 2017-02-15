21155a69d8e178348b5e8d3ba7462f46132e22f6
Sheldon <jangshelldon@gmail.com>
2017-02-16 00:00:05 +0300

    standart interface updates #2

--- extra_buttons_bar/Blizz_interface_improvements.lua
+++ extra_buttons_bar/Blizz_interface_improvements.lua
@@ -5,12 +5,31 @@ if AIO.AddAddon() then
     return
 end
 
+local MicroButtons = {
+        AchievementMicroButton
+        QuestLogMicroButton
+        SocialsMicroButton
+        CharUpdatesMicroButton
+        MainMenuMicroButton
+        HelpMicroButton
+}
+
 BI_i = CreateFrame("Frame", nil, UIParent)
 BI_i:SetScript("OnUpdate", function()
     if (LFDMicroButton:IsVisible()) then
         LFDMicroButton:Hide()
         CharUpdatesMicroButton:Show()
     end
-    
+    if (TalentMicroButton:IsVisible()) then
+        TalentMicroButton:Hide()
+        --moving all buttons to right
+        for i = 1, #MicroButtons do
+            if (i == 1) then
+                MicroButtons[i]:SetPoint("BOTTOMLEFT", SpellbookMicroButton, "BOTTOMRIGHT", -3, 0)
+            else
+            MicroButtons[i]:SetPoint("BOTTOMLEFT", MicroButtons[i-1], "BOTTOMRIGHT", -3, 0)
+        end
+        end
+    end
     end)
 BI_i:Show()
