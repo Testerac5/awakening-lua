ba46e266a08928acb610bcd77cd084a5dcfd1a36
Sheldon <jangshelldon@gmail.com>
2017-02-16 03:05:36 +0300

    Sanctuary fixes

--- Misc/SanctuaryFix.lua
+++ Misc/SanctuaryFix.lua
@@ -11,12 +11,14 @@
 
 ]]
 
---[[local function sanctuary_fix_2(eventId, delay, repeats, player) -- using to prevent saving flags after leaving the inn
-if (not(player:IsRested()) and not(player:IsFFAPVP())) then
+local function sanctuary_fix_2(eventId, delay, repeats, player) -- using to prevent saving flags after leaving the inn
+--[[if (not(player:IsRested()) and not(player:IsFFAPVP())) then
 	player:SetFFA(true)
-end
+end]]--
 
+if (player:IsFFAPVP()) then
 player:SendBroadcastMessage("test") -- DEBUG
+end
 
 --[[if (player:IsInCombat() and player:GetVictim()) then
 	if not(player:GetVictim():ToPlayer()) then
@@ -28,11 +30,11 @@ player:SendBroadcastMessage("test") -- DEBUG
 			player:ClearInCombat()
 		end
 	end
-	end
+	end]]--
 end
 
 local function TimingChecks(event,player)
 	player:RegisterEvent(sanctuary_fix_2, 2000, 0)
 end
 
-RegisterPlayerEvent(3, TimingChecks)]]--
+RegisterPlayerEvent(3, TimingChecks)
