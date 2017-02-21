49d97a8406ef594b582f15fe1d133f02d4e4a41d
Sheldon <jangshelldon@gmail.com>
2017-02-21 04:50:13 +0300

    talent exploit fix

--- extra_buttons_bar/Blizz_interface_improvements.lua
+++ extra_buttons_bar/Blizz_interface_improvements.lua
@@ -44,3 +44,12 @@ BI_i:SetScript("OnUpdate", function()
     end
     end)
 BI_i:Show()
+
+--fix for talent points players have and which can be used in standart interface and then exploited
+local function RemoveTalentPoints(event,player)
+    if (player:GetFreeTalentPoints()) then
+        player:SetFreeTalentPoints(0)
+    end
+end
+
+RegisterPlayerEvent(3, RemoveTalentPoints)
