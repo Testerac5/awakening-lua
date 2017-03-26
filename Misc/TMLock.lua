070e270e6cb639df008ff5efed271b4bb4aeb881
EpitomeRP <epitomerp@gmail.com>
2017-03-26 01:37:56 -0600

    Locks GMs (rank 2) to GMIsland

---
+++ Misc/TMLock.lua
@@ -0,0 +1,20 @@
+local function TMLock1(event, player)
+	if (player:GetGMRank() ~= 2) then
+		return false
+	else
+		player:AddAura(9454, player)
+	end
+end
+
+local function TMLock2(event, player, newZone, newArea)
+	if (player:GetGMRank() ~= 2) then
+		return false
+	elseif (player:GetZoneId() == 876) then
+		return false
+	else
+		player:Teleport(1, 16225.9, 16255, 13.0438, 4.35969)
+	end
+end
+
+RegisterPlayerEvent(27, TMLock2)
+RegisterPlayerEvent(3, TMLock1)
