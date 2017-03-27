d28fde4a98acf734d7eb667b72595b2a9dbd3a2d
Sheldon <jangshelldon@gmail.com>
2017-03-27 04:00:21 +0300

    Gm checks update

--- Misc/TMLock.lua
+++ Misc/TMLock.lua
@@ -1,22 +1,14 @@
-local function TMLock1(event, player)
+local function TMLock1(event, player) -- prevent using spells by gms rank 2
 	if (player:GetGMRank() ~= 2) then
 		return false
 	else
-		player:AddAura(6462, player)
-		player:AddAura(42201, player)
+		--player:AddAura(6462, player)
+		--player:AddAura(42201, player)
+		player:SetFlag(150,0x00800000)
 	end
 end
 
-local function TMLock3(event, player)
-	if (player:GetGMRank() ~= 3) then
-		return false
-	else
-		player:AddAura(6462, player)
-		player:AddAura(42201, player)
-	end
-end
-
-local function TMLock2(event, player, newZone, newArea)
+local function TMLock2(event, player, newZone, newArea) -- Teleport rank2 gms to gmisland
 	if (player:GetGMRank() ~= 2) then
 		return false
 	elseif (player:GetZoneId() == 876) then
@@ -26,6 +18,24 @@ local function TMLock2(event, player, newZone, newArea)
 	end
 end
 
-RegisterPlayerEvent(27, TMLock2)
-RegisterPlayerEvent(3, TMLock1)
-RegisterPlayerEvent(3, TMLock3)
+local function TMLock3(eventId, delay, repeats, player)
+		--player:AddAura(6462, player)
+		--player:AddAura(42201, player)
+		--player:SetFlag(150,0x00800000)
+		if (player:IsGMVisible()) then
+		player:SetGMVisible(false)
+		player:AddAura(37800, player)
+	end
+end
+
+RegisterPlayerEvent(27, TMLock2) -- zonechange event
+RegisterPlayerEvent(3, TMLock1) -- onlogin event
+
+local function TimingChecks_GM(event,player) -- rank 3 .gm vis off all the time
+	if (player:GetGMRank() ~= 3) then
+		return false
+	end
+	player:RegisterEvent(TMLock3, 2000, 0)
+end
+
+RegisterPlayerEvent(3, TimingChecks_GM)
