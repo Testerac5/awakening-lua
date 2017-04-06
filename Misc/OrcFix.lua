b7c1acd84f284da7d5322457fc423947413ec363
Vdad <derekpowellofficial@gmail.com>
2017-04-05 23:48:45 -0500

    This isn't necessary with recent fix. No more orc fix.

--- Misc/OrcFix.lua
+++
@@ -1,9 +0,0 @@
-local function OrcFix(event, player)
-	player:RemoveSpell(20576)
-	player:RemoveSpell(21563)
-	player:RemoveSpell(54562)
-	player:RemoveSpell(33702)
-end
-
-
-RegisterPlayerEvent(3, OrcFix)
