263f4a1d841d65654eb0e06a4e861db75471a872
Sheldon <jangshelldon@gmail.com>
2017-04-18 02:13:31 +0300

    Range action button fix #6

--- TheClassMaskingSolution/TooltipCorrector.lua
+++ TheClassMaskingSolution/TooltipCorrector.lua
@@ -2102,11 +2102,13 @@ function ActionButtonAscension_OnUpdate (self, elapsed)
 			local valid = IsActionInRange(self.action); -- fuck you, valid function. Exception for our solution below
 			 	local type, id, subType, spellID = GetActionInfo(self.action)
 	           	if (SpellCost_ActionButtonAscension[spellID]) then
+	           		if (ActionButtonRange ~= nil) then
 	           		if (SpellCost_ActionButtonAscension[spellID][3] > ActionButtonRange) then
 	           			valid = 1
-	           		elseif (SpellCost_ActionButtonAscension[spellID][3] < ActionButtonRange)
+	           		else
 	           			valid = 0
 	           		end
+	           	end
 	           	end
 		         --
 			if ( count:GetText() == RANGE_INDICATOR ) then
