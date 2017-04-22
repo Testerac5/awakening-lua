466e8c0d8ef552bf6e97e6e7241324bd36ca298a
Vdad <derekpowellofficial@gmail.com>
2017-04-22 03:13:39 -0500

    Fixed Supporter Scroll SP

--- Misc/items/SupporterScroll.lua
+++ Misc/items/SupporterScroll.lua
@@ -1,4 +1,4 @@
-``local function Scroll(event, player, item, target)
+local function Scroll(event, player, item, target)
 	player:SetKnownTitle(258)
 	player:SendNotification("|cffFF6600 Thanks for the Support! |r")
 	player:CastSpell(player, 55420, false)
