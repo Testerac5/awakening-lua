e90ca95d74fdb8217e652027d1f87934b09dd4cb
Sheldon <jangshelldon@gmail.com>
2017-05-05 02:53:44 +0300

    Update of taxi cheat scroll

--- Misc/items/TaxiCheatScroll.lua
+++ Misc/items/TaxiCheatScroll.lua
@@ -1,7 +1,7 @@
 local function TaxiCheatScroll(event, player, item, target)
 	player:SetTaxiCheat(true)
 	player:SendNotification("|cffFF6600 You're now free to choose any flight path you want! |r")
-	player:CastSpell(player, 70571, false)
+	player:CastSpell(player, 51908, false)
 	--player:RemoveAura(70571)
 	if not(CharDBQuery("SELECT guid FROM custom_taxicheat WHERE guid = "..player:GetGUIDLow()..";")) then
 		CharDBExecute("INSERT INTO custom_taxicheat VALUES ("..player:GetGUIDLow()..");")
