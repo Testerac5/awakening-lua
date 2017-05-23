7b56c415d6aeefec359d08dfb2852fba082466c9
Vdad <team@project-ascension.com>
2017-05-23 17:29:53 -0500

    Added Andorhal to the command list

--- update_lua.lua
+++ update_lua.lua
@@ -17,12 +17,12 @@ function update(event, player, msg, Type, lang)
 		elseif msg == "%push dbc sargeras" then
 		
 			os.execute ('bash /home/develop/awake/main/bin/lua_scripts/update_dbc.sh &')
-			player:SendBroadcastMessage("Successfully pushed DBC Files. This requires a restart")
+			player:SendBroadcastMessage("Successfully pushed DBC Files. This requires a restart. This is a stable version push. Don't forget to merge changes in from master.")
 		
 		elseif msg == "%push core sargeras" then
 		
-			os.execute ('bash /home/develop/compile.sh &')
-			player:SendBroadcastMessage("Successfully started compiling core. This requires a restart but please wait a few minutes for compiling to finish.")
+			os.execute ('bash /home/develop/compile_sargeras.sh &')
+			player:SendBroadcastMessage("Successfully started compiling core. This requires a restart but please wait a few minutes for compiling to finish. This is a stable version push. Don't forget to merge changes in from master.")
 
 		elseif msg == "%push lua proudmoore" then
 		
@@ -41,8 +41,30 @@ function update(event, player, msg, Type, lang)
 			
 		elseif msg == "%push core proudmoore" then
 		
-			os.execute ('bash /home/develop/develop.sh &')
+			os.execute ('bash /home/develop/compile_proudmoore.sh &')
 			player:SendBroadcastMessage("Successfully started compiling core. This requires a restart but please wait a few minutes for compiling to finish.")
+				
+		elseif msg == "%push lua andorhal" then
+		
+			os.execute ('bash /home/develop/awake/andorhal/bin/lua_scripts/update_lua.sh &')
+			player:SendBroadcastMessage("Successfully pushed Lua Scripts. Some Modules require restarts or .reload eluna")
+		
+		elseif msg == "%push log andorhal" then
+		
+			os.execute ('bash /home/develop/awake/andorhal/bin/lua_scripts/push_log.sh &')
+			player:SendBroadcastMessage("Successfully pushed Lua Log")
+		
+		elseif msg == "%push dbc andorhal" then
+		
+			os.execute ('bash /home/develop/awake/andorhal/bin/lua_scripts/update_dbc.sh &')
+			player:SendBroadcastMessage("Successfully pushed DBC Files. This requires a restart")
+			
+		elseif msg == "%push core andorhal" then
+		
+			os.execute ('bash /home/develop/compile_andorhal.sh &')
+			player:SendBroadcastMessage("Successfully started compiling core. This requires a restart but please wait a few minutes for compiling to finish.")
+			
+			
 
 		end
     end
